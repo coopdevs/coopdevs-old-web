98195ad10d2d1954fba3e9aaf0ae935ac02a6abd
Pau Perez <saulopefa@gmail.com>
2019-08-29 18:10:18 +0200

    Configure Tailwind with Figma's styleguide
    This copies the values we have set up in the Figma project.

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   theme: {
-    extend: {}
+    color: {
+      gray: {
+        100: '#e5e5e5',
+        300: '#83898a',
+        400: '#727879',
+        700: '#2D4448',
+      },
+      blue: {
+        400: '#39BBD5',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Lato'],
+      },
+    }
   },
   variants: {},
   plugins: []
