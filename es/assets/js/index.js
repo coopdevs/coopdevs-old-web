28aa48eeebb099535d85510ef40581ec0f1a4042
Pau Perez <saulopefa@gmail.com>
2019-09-27 13:35:23 +0200

    Pages built at 22b4ba078daa7bb4769995522cb9292dc520240d (develop)

---
+++ es/assets/js/index.js
@@ -0,0 +1,20 @@
+(function() {
+
+  var hamburger = {
+    openButton: document.querySelector('#sidebar-open'),
+    closeButton: document.querySelector('#sidebar-close'),
+    sidebar: document.querySelector('#sidebar'),
+
+    toggle: function(e) {
+      e.preventDefault();
+      this.openButton.classList.toggle('hidden');
+      this.closeButton.classList.toggle('hidden');
+      this.sidebar.classList.toggle('hidden');
+    }
+  };
+
+  hamburger.openButton.addEventListener('click', function(e) { hamburger.toggle(e); });
+  hamburger.closeButton.addEventListener('click', function(e) { hamburger.toggle(e); });
+  hamburger.sidebar.addEventListener('click', function(e) { hamburger.toggle(e); });
+
+}());
